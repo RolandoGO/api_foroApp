60dcec9b679713a5edd8dffcf7ea27de330c22df
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-08-11 14:58:20 -0300

    adding hash and salt to the passwor before register and handling errors from database

--- server/controls/registerControl/index.js
+++ server/controls/registerControl/index.js
@@ -1,25 +1,40 @@
+import bcryptjs from "bcryptjs"
 import Users from "../../models/UserModel.js"
 
 
 
-// function for controling the insert of the user, passes the error of the database to the next middleware if there is anny
+// function for controling the insert of the user, passes the error of the database to the next middleware if there is anny, hashes the password before insert
 
-export default function registerControl(req,res,next){
+export default async function registerControl(req,res,next){
 
 
     const {name, email, last_name,password} = req.body
+
+    const hashedPassword = await bcryptjs.hash(password.toString(),10)
+
 
     Users.create({
         user_name:name,
         user_last_name:last_name,
         user_email:email,
-        hashedPassword:password
+        hashedPassword
     })
-    .then(result=>{res.json({message:"user created"})})
-    .catch(error=>next(error))
+    .then(result=>{res.json(result)})
+    .catch(error=>{
+        
+        const err = new Error(error.parent.sqlMessage || "database validation error")
+        err.status = 500
+        next(err)
+    })
 
     
 
     
 
 }
+
+    
+
+
+
+
